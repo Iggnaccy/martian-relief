04d57f83aea325291d364de5cfa5bdbdb9d3641b
Iggnaccy <ignas_ignacy@o2.pl>
2016-02-07 16:17:47 +0100

    Ruszający się klocek!
    Do you reallly need more description than that?

---
+++ MartianRelief/Assets/Scripts/TestUserInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestUserInput : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D myBody;
+    private float mySpeedX, mySpeedY;
+
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        mySpeedX = 0;
+        mySpeedY = 0;
+    }
+
+	void Update () 
+	{
+        myBody.velocity = new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime * speed, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+    }
+}
