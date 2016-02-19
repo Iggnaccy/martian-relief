64628257f9c5926d1c891e18417810419ba978b9
Iggnaccy <ignas_ignacy@o2.pl>
2016-02-19 20:10:14 +0100

    Bugfix
    Można już odejść od ściany

--- MartianRelief/Assets/Scripts/BoundaryGlobal.cs
+++ MartianRelief/Assets/Scripts/BoundaryGlobal.cs
@@ -18,15 +18,6 @@ public class BoundaryGlobal : MonoBehaviour {
         x = transform.position.x;
         y = transform.position.y;
         transform.position = new Vector3(Mathf.Clamp(x, xMin, xMax), Mathf.Clamp(y, yMin, yMax), transform.position.z);
-        if (transform.position.x != x) stopX = true;
-        if (transform.position.y != y) stopY = true;
 	}
 
-    void FixedUpdate()
-    {
-        if (stopX)
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0f, GetComponent<Rigidbody2D>().velocity.y);
-        if (stopY)
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0f);
-    }
 }
