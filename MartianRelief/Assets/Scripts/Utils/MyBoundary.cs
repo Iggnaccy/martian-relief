e0e57bf69963ce7fb7543504104ea5d4d3c437fc
Iggnaccy <ignas_ignacy@o2.pl>
2016-03-27 19:03:27 +0200

    Nie wypadam.
    Jestem obiektem Player i od 30 sekund nie wypadam poza mapę.

---
+++ MartianRelief/Assets/Scripts/Utils/MyBoundary.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyBoundary : MonoBehaviour
+{
+    public float minX, maxX, minY, maxY;
+	
+	void Update ()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), Mathf.Clamp(transform.position.y, minY, maxY), 0);
+	}
+}
