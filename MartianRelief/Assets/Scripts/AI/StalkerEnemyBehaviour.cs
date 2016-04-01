34576de11d5159c58caf138da3399673bbde9398
Iggnaccy <ignas_ignacy@o2.pl>
2016-04-02 01:53:12 +0200

    Version #1
    WIDZĘ CIĘ. IDĘ BEZPOŚREDNIO W TWOJĄ STRONĘ.

---
+++ MartianRelief/Assets/Scripts/AI/StalkerEnemyBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StalkerEnemyBehaviour : MonoBehaviour
+{
+
+    Rigidbody2D myBody;
+    Transform toStalk;
+    string toSearch;
+
+	void Start ()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        toSearch = "Player";
+	}
+	
+	void Update ()
+    {
+	    if(toStalk == null)
+        {
+            FindEnemy();
+        }
+	}
+
+    void FixedUpdate()
+    {
+        AdjustVelocity();
+    }
+
+    void FindEnemy()
+    {
+        GameObject[] possibleTargets = GameObject.FindGameObjectsWithTag(toSearch);
+        int randomTarget = Random.Range(0, possibleTargets.GetLength(0) - 1);
+        while (possibleTargets[randomTarget] == this.gameObject)
+        {
+            randomTarget = Random.Range(0, possibleTargets.GetLength(0) - 1);
+        }
+        toStalk = possibleTargets[randomTarget].transform;
+    }
+
+    void AdjustVelocity()
+    {
+        myBody.velocity = (toStalk.position - transform.position).normalized * GetComponent<BasicEnemyStats>().movespeed * Time.deltaTime;
+    }
+}
