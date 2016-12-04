20203df53fc4070df2866b3bcf195871cfbdb2cc
Iggnaccy <ignas_ignacy@o2.pl>
2016-12-04 18:30:57 +0100

    The new Beginning
    pierwsze efekty zrobione, na 90% działają, trzeba jeszcze powiązać z
    itemkami i dodać na playera

--- MartianRelief/Assets/Scripts/Pellets/PelletBehaviour.cs
+++ MartianRelief/Assets/Scripts/Pellets/PelletBehaviour.cs
@@ -6,16 +6,48 @@ public class PelletBehaviour : MonoBehaviour
     
     public float damage, knockback;
     public string targetTag;
+    public SpecialItemEffects myEffects;
     
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == targetTag)
         {
             other.GetComponent<BasicEnemyStats>().health -= damage;
-            // ==> velocity fix
-            if (other.attachedRigidbody)
-                other.attachedRigidbody.velocity += GetComponent<Rigidbody2D>().velocity * 2;
-            Destroy(gameObject);
+            myEffects.OnHitEffects(other.gameObject, gameObject);
+            if((myEffects.myEffects & SpecialItemEffects.Effects.PENETRATIVE) != SpecialItemEffects.Effects.PENETRATIVE)
+            {
+                StopAllCoroutines();
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void Start()
+    {
+        if ((myEffects.myEffects & SpecialItemEffects.Effects.LIGHTNING_BOLT) == SpecialItemEffects.Effects.LIGHTNING_BOLT)
+        {
+            StartCoroutine("Lightning");
+        }
+    }
+
+    IEnumerator Lightning()
+    {
+        Timer timer = new Timer();
+        while(true)    
+        {
+            if (timer.getTime() > 0.5f)
+            {
+                Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, 2f);
+                foreach (Collider2D hit in hits)
+                {
+                    if (hit.CompareTag(targetTag))
+                    {
+                        hit.GetComponent<BasicEnemyStats>().health -= damage / 4;
+                    }
+                }
+                timer.reset();
+            }
+            yield return null;
         }
     }
 }
